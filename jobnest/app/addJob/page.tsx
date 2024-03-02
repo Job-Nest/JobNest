00c92d740c161b6dc0399eff088d0eb90f1758b5
Kevin Chou <choukevin@berkeley.edu>
2024-03-02 00:23:37 -0800

    Rendering JobList on homepage

--- jobnest/app/addJob/page.tsx
+++ jobnest/app/addJob/page.tsx
@@ -1,10 +1,50 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import { useState, useEffect } from 'react';
+
+// try {
+//   fetch('https://www.example.com/submit-form', {
+//     method: 'POST', // Specify the HTTP method
+//     body: new FormData(document.querySelector('form')) // Collect form data
+//   })
+//     .then(response => response.text()) // Read response as text
+//     .then(data => alert(data)); // Alert the response
+// } catch (error) {
+//   alert('An error occurred!');
+// }
+
 export default function addJob() {
-    return (
-      <form>
-        <input type='text' placeholder='Job Title'></input>
-        <input type='text' placeholder='Company Name'></input>
-        <button>Add Job</button>
-      </form>
-    );
-  }
-  
+  const [title, setJobTitle] = useState('');
+  const [company_name, setCompanyName] = useState('');
+  const router = useRouter();
+
+  const addNewJob = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/api/jobs', {
+        method: 'POST',
+        body: JSON.stringify({ title, company_name }),
+      });
+      let result = await response.json();
+      if (result) {
+        router.push('/jobs');
+      }
+    } catch (err) {
+      console.error('Unable to add new job!');
+    }
+  };
+  return (
+    <form onSubmit={addNewJob}>
+      <input
+        type='text'
+        placeholder='Job Title'
+        onChange={(e) => setJobTitle(e.target.value)}
+      ></input>
+      <input
+        type='text'
+        placeholder='Company Name'
+        onChange={(e) => setCompanyName(e.target.value)}
+      ></input>
+      <button>Add Job</button>
+    </form>
+  );
+}
--- jobnest/components/JobListing.tsx
+++ jobnest/components/JobListing.tsx
@@ -1,11 +1,38 @@
 import RemoveBtn from './RemoveBtn';
-import EditBtn from './EditBtn';
 import Link from 'next/link';
+import { useEffect } from 'react';
 
-export default function JobListing() {
+const getJobs = async () => {
+  try {
+    const jobs = await fetch('http://localhost:3000/api/jobs', {
+      cache: 'no-store',
+    });
+    const response = await jobs.json();
+    console.log(response);
+    return response;
+  } catch {
+    console.error('Failed to getJobs');
+  }
+};
+export default async function JobListing() {
+  const { jobs } = await getJobs();
+  console.log(jobs);
+
   return (
     <>
-      <div className='p-4 border border-slate-300 flex justify-between items-center '>
+      {jobs.map((e) => (
+        <div className='p-4 border border-slate-300 flex justify-between items-center '>
+          <div>
+            <div>{e.title}</div>
+            <div>{e.company_name}</div>
+          </div>
+          <div>
+            <RemoveBtn />
+            <Link href={`/editJob/${e._id}`}>Edit</Link>
+          </div>
+        </div>
+      ))}
+      {/* <div className='p-4 border border-slate-300 flex justify-between items-center '>
         <div>
           <div>Job Listing</div>
         </div>
@@ -13,7 +40,7 @@ export default function JobListing() {
           <RemoveBtn />
           <Link href='/editJob/1'>Edit</Link>
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
